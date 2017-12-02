c36093e51f2cbf676614b7ebf74f9f879d203907
guri1026 <34181727+guri1026@users.noreply.github.com>
2017-12-02 17:23:57 +0900

    Main

---
+++ git_testcode/git_testcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git_testcode
+//
+//  Created by 臼井奎太 on 2017/12/02.
+//  Copyright © 2017年 臼井奎太. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
